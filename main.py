351c16532d5fbeac2de1cbfc9c32a76987b22f34
Arthur Robin <robina@educ.college-montreal.qc.ca>
2023-05-03 08:22:45 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import random as r
+import arcade as a
+import arcade.gui as gui
+from attack_animation import AttackType, AttackAnimation
+from game_state import GameState
+
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Roche,papier, ciseaux"
+DEFAULT_LINE_HEIGHT = 45
